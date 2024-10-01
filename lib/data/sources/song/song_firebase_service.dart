684a455e5d21b65ceea2a4495ae87de196790293
arshadkusman <arshadkk892@gmail.com>
2024-10-01 12:24:54 +0530

    news songs limit increased

--- lib/data/sources/song/song_firebase_service.dart
+++ lib/data/sources/song/song_firebase_service.dart
@@ -8,7 +8,6 @@ import 'package:spotify/domain/usecases/song/is_favorite_song.dart';
 import '../../../service_locator.dart';
 
 abstract class SongFirebaseService {
-
   Future<Either> getNewsSongs();
   Future<Either> getPlayList();
   Future<Either> addOrRemoveFavoriteSong(String songId);
@@ -16,169 +15,146 @@ abstract class SongFirebaseService {
   Future<Either> getUserFavoriteSongs();
 }
 
-
 class SongFirebaseServiceImpl extends SongFirebaseService {
-
-  
   @override
-  Future < Either > getNewsSongs() async {
+  Future<Either> getNewsSongs() async {
     try {
-      List < SongEntity > songs = [];
-      var data = await FirebaseFirestore.instance.collection('Songs')
-        .orderBy('releaseDate', descending: true)
-        .limit(3)
-        .get();
+      List<SongEntity> songs = [];
+      var data = await FirebaseFirestore.instance
+          .collection('Songs')
+          .orderBy('releaseDate', descending: true)
+          .limit(10)
+          .get();
 
       for (var element in data.docs) {
         var songModel = SongModel.fromJson(element.data());
-        bool isFavorite = await sl<IsFavoriteSongUseCase>().call(
-          params: element.reference.id 
-        );
+        bool isFavorite = await sl<IsFavoriteSongUseCase>()
+            .call(params: element.reference.id);
         songModel.isFavorite = isFavorite;
         songModel.songId = element.reference.id;
-        songs.add(
-          songModel.toEntity()
-        );
+        songs.add(songModel.toEntity());
       }
 
       return Right(songs);
-
     } catch (e) {
       print(e);
       return const Left('An error occurred, Please try again.');
     }
-
   }
-  
+
   @override
   Future<Either> getPlayList() async {
-     try {
-      List < SongEntity > songs = [];
-      var data = await FirebaseFirestore.instance.collection('Songs')
-        .orderBy('releaseDate', descending: true)
-        .get();
+    try {
+      List<SongEntity> songs = [];
+      var data = await FirebaseFirestore.instance
+          .collection('Songs')
+          .orderBy('releaseDate', descending: true)
+          .get();
 
       for (var element in data.docs) {
         var songModel = SongModel.fromJson(element.data());
-        bool isFavorite = await sl<IsFavoriteSongUseCase>().call(
-          params: element.reference.id 
-        );
+        bool isFavorite = await sl<IsFavoriteSongUseCase>()
+            .call(params: element.reference.id);
         songModel.isFavorite = isFavorite;
         songModel.songId = element.reference.id;
-        songs.add(
-          songModel.toEntity()
-        );
+        songs.add(songModel.toEntity());
       }
 
       return Right(songs);
-
     } catch (e) {
       print(e);
       return const Left('An error occurred, Please try again.');
     }
   }
-  
+
   @override
   Future<Either> addOrRemoveFavoriteSong(String songId) async {
-
     try {
-    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-    final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-    
-    late bool isFavorite;
-    var user = firebaseAuth.currentUser;
-    String uId = user!.uid;
+      final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+      final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
-   QuerySnapshot favoriteSongs = await  firebaseFirestore.collection('Users')
-    .doc(uId)
-    .collection('Favorites')
-    .where(
-      'songId',isEqualTo: songId
-    ).get();
+      late bool isFavorite;
+      var user = firebaseAuth.currentUser;
+      String uId = user!.uid;
 
-    if(favoriteSongs.docs.isNotEmpty) {
-      await favoriteSongs.docs.first.reference.delete();
-      isFavorite = false;
-    } else {
-      await firebaseFirestore.collection('Users')
-      .doc(uId)
-      .collection('Favorites')
-      .add(
-        {
-          'songId' : songId,
-          'addedDate' : Timestamp.now()
-        }
-      );
-      isFavorite = true;
+      QuerySnapshot favoriteSongs = await firebaseFirestore
+          .collection('Users')
+          .doc(uId)
+          .collection('Favorites')
+          .where('songId', isEqualTo: songId)
+          .get();
+
+      if (favoriteSongs.docs.isNotEmpty) {
+        await favoriteSongs.docs.first.reference.delete();
+        isFavorite = false;
+      } else {
+        await firebaseFirestore
+            .collection('Users')
+            .doc(uId)
+            .collection('Favorites')
+            .add({'songId': songId, 'addedDate': Timestamp.now()});
+        isFavorite = true;
+      }
+
+      return Right(isFavorite);
+    } catch (e) {
+      return const Left('An error occurred');
     }
-
-    return Right(isFavorite);
-    
-    
-  } catch(e){
-    return const Left('An error occurred');
   }
-}
 
   @override
   Future<bool> isFavoriteSong(String songId) async {
     try {
-    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-    final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-    var user = firebaseAuth.currentUser;
-    String uId = user!.uid;
+      final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+      final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+      var user = firebaseAuth.currentUser;
+      String uId = user!.uid;
 
-   QuerySnapshot favoriteSongs = await  firebaseFirestore.collection('Users')
-    .doc(uId)
-    .collection('Favorites')
-    .where(
-      'songId',isEqualTo: songId
-    ).get();
+      QuerySnapshot favoriteSongs = await firebaseFirestore
+          .collection('Users')
+          .doc(uId)
+          .collection('Favorites')
+          .where('songId', isEqualTo: songId)
+          .get();
 
-    if(favoriteSongs.docs.isNotEmpty) {
-      return true;
-    } else {
+      if (favoriteSongs.docs.isNotEmpty) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
       return false;
     }
-  } catch(e){
-    return false;
   }
-  }
-  
+
   @override
-  Future < Either > getUserFavoriteSongs() async {
+  Future<Either> getUserFavoriteSongs() async {
     try {
       final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
       final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
       var user = firebaseAuth.currentUser;
       List<SongEntity> favoriteSongs = [];
       String uId = user!.uid;
-      QuerySnapshot favoritesSnapshot = await firebaseFirestore.collection(
-        'Users'
-      ).doc(uId)
-      .collection('Favorites')
-      .get();
-      
-      for (var element in favoritesSnapshot.docs) { 
+      QuerySnapshot favoritesSnapshot = await firebaseFirestore
+          .collection('Users')
+          .doc(uId)
+          .collection('Favorites')
+          .get();
+
+      for (var element in favoritesSnapshot.docs) {
         String songId = element['songId'];
-        var song = await firebaseFirestore.collection('Songs').doc(songId).get();
+        var song =
+            await firebaseFirestore.collection('Songs').doc(songId).get();
         SongModel songModel = SongModel.fromJson(song.data()!);
         songModel.isFavorite = true;
         songModel.songId = songId;
-        favoriteSongs.add(
-          songModel.toEntity()
-        );
+        favoriteSongs.add(songModel.toEntity());
       }
-      
-      return Right(favoriteSongs);
 
+      return Right(favoriteSongs);
     } catch (e) {
       print(e);
-      return const Left(
-        'An error occurred'
-      );
+      return const Left('An error occurred');
     }
   }
-
-  
 }
--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -17,104 +17,98 @@ class NewsSongs extends StatelessWidget {
     return BlocProvider(
       create: (_) => NewsSongsCubit()..getNewsSongs(),
       child: SizedBox(
-        height: 200,
-        child: BlocBuilder<NewsSongsCubit,NewsSongsState>(
-          builder: (context,state) {
-            if (state is NewsSongsLoading) {
-              return Container(
-                alignment: Alignment.center,
-                child: const CircularProgressIndicator()
-              );
-            } 
+          height: 200,
+          child: BlocBuilder<NewsSongsCubit, NewsSongsState>(
+            builder: (context, state) {
+              if (state is NewsSongsLoading) {
+                return Container(
+                    alignment: Alignment.center,
+                    child: const CircularProgressIndicator());
+              }
 
-            if (state is NewsSongsLoaded) {
-              return _songs(
-                state.songs
-              );
-            }
+              if (state is NewsSongsLoaded) {
+                return _songs(state.songs);
+              }
 
-            return Container();
-          },
-        )
-        
-      ),
+              return Container();
+            },
+          )),
     );
   }
 
   Widget _songs(List<SongEntity> songs) {
     return ListView.separated(
-      scrollDirection: Axis.horizontal,
-      shrinkWrap: true,
-      itemBuilder: (context,index) {
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-               builder: (BuildContext context) 
-              => SongPlayerPage(
-                songEntity: songs[index],
-              )
-            )
-            );
-          },
-          child: SizedBox(
-            width: 160,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          '${AppURLs.coverFirestorage}${songs[index].artist} - ${songs[index].title}.jpg?${AppURLs.mediaAlt}'
-                        )
-                      )
-                    ),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        transform: Matrix4.translationValues(10, 10, 0),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: context.isDarkMode ?  AppColors.darkGrey : const Color(0xffE6E6E6)
-                        ),
-                        child: Icon(
-                          Icons.play_arrow_rounded,
-                          color: context.isDarkMode ? const Color(0xff959595) : const Color(0xff555555),
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => SongPlayerPage(
+                            songEntity: songs[index],
+                          )));
+            },
+            child: SizedBox(
+              width: 160,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  '${AppURLs.coverFirestorage}${songs[index].artist} - ${songs[index].title}.jpg?${AppURLs.mediaAlt}'))),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          transform: Matrix4.translationValues(10, 10, 0),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: context.isDarkMode
+                                  ? AppColors.darkGrey
+                                  : const Color(0xffE6E6E6)),
+                          child: Icon(
+                            Icons.play_arrow_rounded,
+                            color: context.isDarkMode
+                                ? const Color(0xff959595)
+                                : const Color(0xff555555),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 10,),
-                Text(
-                  songs[index].title,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16
+                  const SizedBox(
+                    height: 10,
                   ),
-                ) ,
-                const SizedBox(height: 5,),
-                Text(
-                  songs[index].artist,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 12
+                  Text(
+                    songs[index].title,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w600, fontSize: 16),
                   ),
-                )
-              ],
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    songs[index].artist,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w400, fontSize: 12),
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      },
-      separatorBuilder: (context,index) => const SizedBox(width: 14,),
-      itemCount: songs.length
-    );
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(
+              width: 14,
+            ),
+        itemCount: songs.length);
   }
 }
